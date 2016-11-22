64230a7db15d71b538c13e561f038fa4708bd30b
meiremans <nick.meiremans@gmail.com>
2016-11-22 16:13:39 +0200

    revert

--- js/components/hero.jsx
+++ js/components/hero.jsx
@@ -1,30 +1,25 @@
 import React from "react";
-import {Container, Row, Col} from "../helpers/bootstrap";
+import { Container, Row, Col } from "../helpers/bootstrap";
 import classNames from "classnames";
 
 export class Hero extends React.Component {
 
-    static propTypes = {
-        backgroundImage: React.PropTypes.string,
-    };
+  static propTypes = {
+    backgroundImage: React.PropTypes.string,
+  };
 
-    render() {
-        const _style = {};
-        if (this.props.backgroundImage) {
-            _style.backgroundImage = `url(${this.props.backgroundImage})`;
-        }
-        const _className = classNames("neal-hero jumbotron jumbotron-fluid zoom_image", this.props.className);
-        const _classNameContent = classNames("neal-hero jumbotron jumbotron-fluid", this.props.className);
-        return (
-            <div>
-                <div className={_className} style={_style}>
-                </div>
-                <div {... this.props} className={_classNameContent}>
-                </div>
-                <Container>
-                    { this.props.children }
-                </Container>
-            </div>
-        );
+  render() {
+    const _style = {};
+    if (this.props.backgroundImage) {
+      _style.backgroundImage = `url(${this.props.backgroundImage})`;
     }
+    const _className = classNames("neal-hero jumbotron jumbotron-fluid", this.props.className);
+    return (
+      <div {... this.props} className={_className} style={_style}>
+        <Container>
+          { this.props.children }
+        </Container>
+      </div>
+    );
+  }
 }
